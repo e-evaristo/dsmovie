b363b183c27a46e8c0a024bdba3568fafe3bfcbe
e-evaristo <emanoelevaristo@hotmail.com>
2022-03-07 19:10:00 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,5 @@
+export default function Form() {
+    return (
+        <h2>Página de formulário</h2>
+    );
+}
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,5 @@
+export default function Listing() {
+    return (
+        <h2>Listando</h2>
+    );
+}
