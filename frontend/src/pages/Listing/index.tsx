3bee6b0499535e5315d309c067816c193ecab20b
e-evaristo <emanoelevaristo@hotmail.com>
2022-03-12 10:34:46 -0300

    Show score

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -22,10 +22,11 @@ export default function Listing() {
     });
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=id`)
             .then(response => {
                 const data = response.data as MoviePage;
                 setPage(data);
+                setPageNumber(pageNumber);
             });
     }, [pageNumber]);
     
